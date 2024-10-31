83791ea0ab0a3391c0cf6a3f6ae7426f2719474a
Jeremy Vajko <jeremy.vajko@gmail.com>
2024-10-30 20:15:23 -0700

    Initial Commit

---
+++ PaymentApp/PaymentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentAppApp.swift
+//  PaymentApp
+//
+//  Created by user on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PaymentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
